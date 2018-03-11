c86c74b8809452ba477d171e0f21d16b437eb72a
derekjackson-hbp <derek.jackson@hbsp.harvard.edu>
2018-03-11 15:48:34 -0400

    accessible menu, adding event listeners

--- js/app.js
+++ js/app.js
@@ -1,5 +1,63 @@
-console.log('here');
-$('#main-menu-list').on('click','li', function(){
-	alert ('clicked')
-})
+var tell = function(str){
+	alert(str)
+}
+
+var app = {
+	menu: function(el) {
+		$(el)
+			.on('click','li',function(){ 
+				var text = $(this).text(); 
+				tell(text);
+			})
+			.keypress(function(e){
+
+				var key = e.keyCode;
+				{debugger}
+				var text = $('*:focus').text();
+				var currEl = $('*:focus')
+				{debugger};
+				switch (key){
+					case 13:
+					case 27: {
+						tell(text);
+						break;
+					}
+					case 38:
+						alert('up');
+						break;
+					case 40:
+						alert ('down');
+						break;	
+				} 
+			})
+			.keydown(function(e){
+				var key = e.keyCode;
+				{debugger}
+				var text = $('*:focus').text();
+				var currEl = $('*:focus')
+				{debugger};
+				switch (key){
+					case 13:
+					case 27:
+						tell(text)
+						break
+					case 38:
+						event.prevetDefault();
+						alert('up');
+						break;
+					case 40:
+						alert ('down');
+						break;	
+				} 
+			})
+	}
+}
+
+
+app.menu('#main-menu-list');
+	
+
+
+
+
 
